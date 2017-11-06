9b44e833aecf88583e4b653a97321c7ab06a2d21
John Munyi <wachirawamunyi@gmail.com>
2017-11-06 23:17:31 +0200

    initialized locations and set up UI

--- app/src/main/java/com/example/johnmunyi/locationwithoutmaps/MainActivity.java
+++ app/src/main/java/com/example/johnmunyi/locationwithoutmaps/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.johnmunyi.locationwithoutmaps;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //finding the text views
+        TextView tvLatitude = (TextView) findViewById(R.id.tvLatitude);
+        TextView tvLongitude = (TextView) findViewById(R.id.tvLongitude);
+        Location loc = null;
+
+        //get handle for location manager
+        LocationManager lm = (LocationManager) getSystemService(getApplicationContext()
+                .LOCATION_SERVICE);
+
+        //connect to the GPS location service
+        if (lm != null) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            loc = lm.getLastKnownLocation("gps");
+        }
+
+        //fill in the TextViews
+        tvLatitude.setText(Double.toString(loc.getLatitude()));
+        tvLongitude.setText(Double.toString(loc.getLongitude()));
     }
 }
