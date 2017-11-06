2ab594cd67cae3ece78e2b407c35f29607663cce
John Munyi <wachirawamunyi@gmail.com>
2017-11-07 00:29:20 +0200

    refactored to big variable init bug

--- app/src/main/java/com/example/johnmunyi/locationwithoutmaps/MainActivity.java
+++ app/src/main/java/com/example/johnmunyi/locationwithoutmaps/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.johnmunyi.locationwithoutmaps;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -10,20 +11,27 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //finding the text views
+    private TextView tvLatitude;
+    private TextView tvLongitude;
+    private TextView latitudeLabel;
+    private TextView longitudeLabel;
+    private Location loc;
+    private LocationListener locListenD;
+    private LocationManager lm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //finding the text views
-        TextView tvLatitude = (TextView) findViewById(R.id.tvLatitude);
-        TextView tvLongitude = (TextView) findViewById(R.id.tvLongitude);
-        Location loc = null;
-
         //get handle for location manager
-        LocationManager lm = (LocationManager) getSystemService(getApplicationContext()
+        lm = (LocationManager) getSystemService(getApplicationContext()
                 .LOCATION_SERVICE);
+        tvLatitude = (TextView) findViewById(R.id.tvLatitude);
+        tvLongitude = (TextView) findViewById(R.id.tvLongitude);
+        latitudeLabel = (TextView) findViewById(R.id.latitudeLabel);
+        longitudeLabel = (TextView) findViewById(R.id.longitudeLabel);
 
         //connect to the GPS location service
         if (lm != null) {
@@ -43,5 +51,33 @@ public class MainActivity extends AppCompatActivity {
         //fill in the TextViews
         tvLatitude.setText(Double.toString(loc.getLatitude()));
         tvLongitude.setText(Double.toString(loc.getLongitude()));
+
+        locListenD = new DispLocListener();
+        lm.requestLocationUpdates("gps", 30000L, 10.0f, locListenD);
+    }
+
+    private class DispLocListener implements LocationListener{
+
+        @Override
+        public void onLocationChanged(Location location) {
+            //update textviews
+            tvLatitude.setText(Double.toString(location.getLatitude()));
+            tvLongitude.setText(Double.toString(location.getLongitude()));
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
     }
 }
