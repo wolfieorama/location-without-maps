53c6e02cd80280e3b5df395299890c54ad9ea7da
John Munyi <wachirawamunyi@gmail.com>
2017-11-07 00:59:34 +0200

    fix for permission check

--- app/src/main/java/com/example/johnmunyi/locationwithoutmaps/MainActivity.java
+++ app/src/main/java/com/example/johnmunyi/locationwithoutmaps/MainActivity.java
@@ -56,6 +56,26 @@ public class MainActivity extends AppCompatActivity {
         lm.requestLocationUpdates("gps", 30000L, 10.0f, locListenD);
     }
 
+    public void onpause() {
+        super.onPause();
+        lm.removeUpdates(locListenD);
+    }
+
+    public void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        lm.requestLocationUpdates("gps", 30000L, 10.0f, locListenD);
+    }
+
     private class DispLocListener implements LocationListener{
 
         @Override
@@ -66,17 +86,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
+        public void onStatusChanged(String provider, int status, Bundle extras) {
 
         }
 
         @Override
-        public void onProviderEnabled(String s) {
+        public void onProviderEnabled(String provider) {
 
         }
 
         @Override
-        public void onProviderDisabled(String s) {
+        public void onProviderDisabled(String provider) {
 
         }
     }
